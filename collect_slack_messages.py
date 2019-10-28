3a2e6b2f5baad9d588bd66ef60acee2fb5815e55
junho85 <junho85@gmail.com>
2019-10-28 11:18:20 +0900

    add generate_attendence_csv.py

--- collect_slack_messages.py
+++ collect_slack_messages.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta, datetime
 from garden import Garden
 
 oldest = datetime(2019, 10, 27).timestamp()
-latest = datetime(2019, 10, 28).timestamp()
+latest = datetime(2019, 10, 29).timestamp()
 
 garden = Garden()
 garden.collect_slack_messages(oldest, latest)
