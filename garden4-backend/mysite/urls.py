b55250e73008a6c5a0f83c15975b3da76e2b77b1
junho85 <junho85@gmail.com>
2019-11-02 17:40:53 +0900

    root path 접근시 attendance 로 리다이렉트

--- garden4-backend/mysite/urls.py
+++ garden4-backend/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('attendance/', include('attendance.urls')),
+    path('', RedirectView.as_view(url="/attendance/")),
 ]
