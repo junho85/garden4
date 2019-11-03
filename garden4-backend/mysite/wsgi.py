2361b00661a132a5ebff30da3401cce1a9a319af
junho85 <junho85@gmail.com>
2019-11-03 16:39:30 +0900

    wsgi config for static file

--- garden4-backend/mysite/wsgi.py
+++ garden4-backend/mysite/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
