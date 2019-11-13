feb6821e233fbb7f66caa419b2a7cf4bc064e990
junho85 <junho85@gmail.com>
2019-11-13 21:46:38 +0900

    진행률 progress bar 추가 레이아웃 조정

--- garden4-backend/attendance/views.py
+++ garden4-backend/attendance/views.py
@@ -23,6 +23,7 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
+
 # 유저별 출석부
 def user(request, user):
     context = {
