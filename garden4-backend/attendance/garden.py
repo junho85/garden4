aed4d1f8491411d04070fe5f5582a8d5d9e3f14c
june.kim(김준호)/kakao <june.kim@kakaocorp.com>
2025-06-28 16:14:15 +0900

    refactor: migrate from MongoDB to PostgreSQL for slack_messages storage
    Replaced MongoDB usage with PostgreSQL, updated relevant database connection logic, and modified methods to use PostgreSQL queries. Updated dependencies in `requirements.txt` to include `psycopg2` and removed `pymongo`. Added timezone handling with `pytz`.

--- garden4-backend/attendance/garden.py
+++ garden4-backend/attendance/garden.py
@@ -1,10 +1,13 @@
 import configparser
 from datetime import date, timedelta, datetime
 from slack_sdk import WebClient
-import pymongo
+import psycopg2
+import psycopg2.extras
+import json
 import pprint
 import os
 import yaml
+import pytz
 
 class Garden:
     def __init__(self):
@@ -18,14 +21,15 @@ class Garden:
 
         self.channel_id = config['DEFAULT']['CHANNEL_ID']
 
-        self.mongo_database = config['MONGO']['DATABASE']
-        self.mongo_host = config['MONGO']['HOST']
-        self.mongo_port = config['MONGO']['PORT']
+        # PostgreSQL settings
+        self.pg_database = config['POSTGRESQL']['DATABASE']
+        self.pg_host = config['POSTGRESQL']['HOST']
+        self.pg_port = config['POSTGRESQL']['PORT']
+        self.pg_user = config['POSTGRESQL']['USER']
+        self.pg_password = config['POSTGRESQL']['PASSWORD']
+        self.pg_schema = config['POSTGRESQL']['SCHEMA']
 
         self.gardening_days = config['DEFAULT']['GARDENING_DAYS']
-
-        # mongodb collections
-        self.mongo_collection_slack_message = "slack_messages"
 
         # users list ['junho85', 'user2', 'user3']
         self.users = config['GITHUB']['USERS'].split(',')
@@ -37,9 +41,24 @@ class Garden:
             self.users_with_slackname = yaml.safe_load(file)
 
         self.start_date = datetime.strptime(config['DEFAULT']['START_DATE'], "%Y-%m-%d").date()  # start_date e.g.) 2019-10-01
+        
+        # 타임존 설정
+        self.kst = pytz.timezone('Asia/Seoul')
 
-    def connect_mongo(self):
-        return pymongo.MongoClient("mongodb://%s:%s" % (self.mongo_host, self.mongo_port))
+    def connect_postgres(self):
+        conn = psycopg2.connect(
+            host=self.pg_host,
+            port=self.pg_port,
+            database=self.pg_database,
+            user=self.pg_user,
+            password=self.pg_password,
+            sslmode='require'
+        )
+        # 연결 후 스키마 설정
+        cursor = conn.cursor()
+        cursor.execute(f"SET search_path TO {self.pg_schema}")
+        cursor.close()
+        return conn
 
     def get_member(self):
         return self.users
@@ -60,33 +79,60 @@ class Garden:
         print(latest)
         print(datetime.fromtimestamp(latest))
 
-        conn = self.connect_mongo()
+        conn = self.connect_postgres()
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-        db = conn.get_database(self.mongo_database)
-        mongo_collection = db.get_collection(self.mongo_collection_slack_message)
+        query = """
+            SELECT ts, ts_for_db
+            FROM slack_messages 
+            WHERE ts_for_db >= %s AND ts_for_db < %s
+        """
+        
+        cursor.execute(query, (datetime.fromtimestamp(oldest), datetime.fromtimestamp(latest)))
+        messages = cursor.fetchall()
 
-        for message in mongo_collection.find({"ts_for_db": {"$gte": datetime.fromtimestamp(oldest), "$lt": datetime.fromtimestamp(latest)}}):
+        for message in messages:
             print(message["ts"])
             print(message)
+
+        cursor.close()
+        conn.close()
 
     # 특정 유저의 전체 출석부를 생성함
     # TODO 출석부를 DB에 넣고 마지막 생성된 출석부 이후의 데이터로 추가 출석부 만들도록 하자
     def find_attendance_by_user(self, user):
-        conn = self.connect_mongo()
+        conn = self.connect_postgres()
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-        db = conn.get_database(self.mongo_database)
-        mongo_collection = db.get_collection(self.mongo_collection_slack_message)
+        # PostgreSQL 쿼리: JSONB 배열에서 author_name이 일치하는 메시지 찾기
+        query = """
+            SELECT ts, ts_for_db, attachments
+            FROM slack_messages 
+            WHERE attachments @> %s
+            ORDER BY ts
+        """
+        
+        # JSONB 쿼리 파라미터
+        param = json.dumps([{"author_name": user}])
+        
+        cursor.execute(query, (param,))
+        messages = cursor.fetchall()
 
         result = {}
-
         start_date = self.start_date
-        for message in mongo_collection.find({"attachments.author_name": user}).sort("ts", 1):
+        
+        for message in messages:
             # make attend
             commits = []
-            for attachment in message["attachments"]:
-                commits.append(attachment["text"])
-            # ts_datetime = datetime.fromtimestamp(float(message["ts"]))
-            ts_datetime = message["ts_for_db"]
+            attachments = message['attachments']
+            if attachments:
+                for attachment in attachments:
+                    if attachment.get('author_name') == user:
+                        commits.append(attachment.get('text', ''))
+            
+            # DB의 ts_for_db는 KST 시간이 UTC로 저장되어 있으므로 9시간을 빼서 올바른 KST로 변환
+            ts_datetime_raw = message['ts_for_db']
+            ts_datetime = ts_datetime_raw - timedelta(hours=9)
             attend = {"ts": ts_datetime, "message": commits}
 
             # current date and date before day1
@@ -105,16 +151,12 @@ class Garden:
 
                 result[date].append(attend)
 
+        cursor.close()
+        conn.close()
         return result
 
-    # github 봇으로 모은 slack message 들을 slack_messages collection 에 저장
+    # github 봇으로 모은 slack message 들을 slack_messages 테이블에 저장
     def collect_slack_messages(self, oldest, latest):
-        # print(oldest)
-        # print(datetime.fromtimestamp(oldest))
-        # print(latest)
-        # print(datetime.fromtimestamp(latest))
-        # print(self.channel_id)
-
         response = self.slack_client.conversations_history(
             channel=self.channel_id,
             latest=str(latest),
@@ -122,30 +164,51 @@ class Garden:
             limit=1000
         )
 
-        conn = self.connect_mongo()
-
-        db = conn.get_database(self.mongo_database)
-        mongo_collection = db.get_collection(self.mongo_collection_slack_message)
+        conn = self.connect_postgres()
+        cursor = conn.cursor()
 
         for message in response["messages"]:
-            message["ts_for_db"] = datetime.fromtimestamp(float(message["ts"]))
-            # pprint.pprint(message)
-
+            ts_for_db = datetime.fromtimestamp(float(message["ts"]))
+            
+            # PostgreSQL INSERT 쿼리
+            insert_query = """
+                INSERT INTO slack_messages (
+                    ts, ts_for_db, bot_id, type, text, "user", team, 
+                    bot_profile, attachments
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s
+                ) ON CONFLICT (ts) DO NOTHING
+            """
+            
             try:
-                mongo_collection.insert_one(message)
-            except pymongo.errors.DuplicateKeyError as err:
-                print(err)
+                cursor.execute(insert_query, (
+                    message.get("ts"),
+                    ts_for_db,
+                    message.get("bot_id"),
+                    message.get("type"),
+                    message.get("text"),
+                    message.get("user"),
+                    message.get("team"),
+                    json.dumps(message.get("bot_profile")) if message.get("bot_profile") else None,
+                    json.dumps(message.get("attachments")) if message.get("attachments") else None
+                ))
+            except Exception as err:
+                print(f"Error inserting message: {err}")
                 continue
 
+        conn.commit()
+        cursor.close()
+        conn.close()
+
     def remove_all_slack_messages(self):
-        conn = self.connect_mongo()
-
-        mongo_database = self.mongo_database
-
-        db = conn.get_database(mongo_database)
-
-        mongo_collection = db.get_collection(self.mongo_collection_slack_message)
-        mongo_collection.delete_many({})
+        conn = self.connect_postgres()
+        cursor = conn.cursor()
+        
+        cursor.execute("DELETE FROM slack_messages")
+        conn.commit()
+        
+        cursor.close()
+        conn.close()
 
     """
     특정일의 출석 데이터 불러오기
